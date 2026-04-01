949fc3ba2c84095cca0593a3ce3e55e592d2fbc1
MetehanSobaci <metehan.sobaci@student.hu.nl>
2026-04-01 20:27:56 +0200

    Commit met w8woorden voor DB
    configuratie werkt hiermee en er is een database connectie

--- practicum/src/main/java/globals/Psql.java
+++ practicum/src/main/java/globals/Psql.java
@@ -4,5 +4,7 @@ public class Psql {
 
     // Naam van de db voor psql-tests, ben je geheel vrij in. Want het testraamwerk tuigt de db op en af
     public static final String dbName = "ovchippsql";
-
+    private String url = "jdbc:postgresql://localhost:5421/ovchip";
+    private String user = "postgres";
+    private String pass = "Ikbensuperman@321";
 }
